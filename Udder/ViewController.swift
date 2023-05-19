22f88f92a9f5032176aa86bbadba763a5fb9356d
LeDuyTan <tanb1910699@student.ctu.edu.vn>
2023-05-19 17:50:46 +0700

    Add more view

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+            
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+    }
 
 }
 
